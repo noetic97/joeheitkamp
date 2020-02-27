5a00c07a3bb558ec13f5ed683f3852e353cf1d48
Joe Heitkamp <noetic97@gmail.com>
2020-02-26 23:44:24 -0700

    Add initial main components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import './App.scss';
+import { Router, Link } from "@reach/router";
+import Home from "../Home/Home";
+import NotFound404 from "../NotFound404/NotFound404";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+// import './App.scss';
 
 function App() {
   return (
     <div className="App">
+    <Header />
+    <Router>
+      <NotFound404 default/>
+      <Home path="/" />
+    </Router>
+    <Footer />
     </div>
   );
 }
---
+++ src/components/NotFound404/NotFound404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NotFound404() {
+  return (
+    <main className="NotFound404">
+      <div>
+        <p>I'M LOST</p>
+      </div>
+    </main>
+  );
+}
+
+export default NotFound404;
